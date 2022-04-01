33b88feafc565a69e4a2655f1aa87fde7feee0ea
Victor Nunes <victor.22072005@gmail.com>
2022-04-01 16:29:57 -0300

    adição de Botoẽs

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import Cabecalho from './src/componets/cabecalho';
+import React from './src/componets/botao';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Botao from './src/componets/botao';
 
 export default function App() {
   return (
     <View>
       <Cabecalho></Cabecalho>
+    <Botao Logo="game-controller" 
+           texto="Jogos" 
+           cor="black">
+      </Botao>
 
 
     </View>
